ed10627e43504e6adef615b93f654537e448c6d7
Omar Torres <torresomar44@gmail.com>
2018-05-18 10:45:11 -0500

    Create API endpoint for signin

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   scope '/api' do
     namespace :users do
       post 'signup' => 'registrations#create'
+      post 'signin' => 'sessions#create'
     end
   end
 
