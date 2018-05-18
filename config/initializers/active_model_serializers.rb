a0e235a4fdc1042db875d220c3bc8f1a0efa5e50
Omar Torres <torresomar44@gmail.com>
2018-05-18 10:37:47 -0500

    Add API endpoint for user signup

---
+++ config/initializers/active_model_serializers.rb
@@ -0,0 +1,5 @@
+# Response document will have a root key
+ActiveModelSerializers.config.adapter = :json
+
+# JSON response will have camelCased keys
+ActiveModelSerializers.config.key_transform = :camel_lower
