f53ee5d6606d09318afcf9aa1ce607077e1110b9
Omar Torres <torresomar44@gmail.com>
2018-05-22 15:29:08 -0500

    fix issue q
    exi
    exit
    
    z
    x
    w

--- app/serializers/doctor_serializer.rb
+++ app/serializers/doctor_serializer.rb
@@ -1,7 +1,7 @@
 class DoctorSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :center_id, :department_id
 
-  belongs_to :center
+  has_many :centers
   belongs_to :department
   has_many :appointments
   has_many :users, through: :appointments
---
+++ db/migrate/20180522202503_add_center_id_again2_to_doctors.rb
@@ -0,0 +1,5 @@
+class AddCenterIdAgain2ToDoctors < ActiveRecord::Migration[5.2]
+  def change
+    add_column :doctors, :center_id, :integer
+  end
+end
